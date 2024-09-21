800fea6db5921af4f4c979be5dd8412efebb0007
88KMA <maximkozlov88@gmail.com>
2024-09-22 00:31:56 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,4 @@
+number_of_completed_HA = 12
+number_of_hours_spend = 1.5
+course_name = 'Python'
+print('Курс:',course_name + ',','всего задач:',str(number_of_completed_HA)+',','затрачено часов:',str(number_of_hours_spend)+',', 'среднее время выполнения:', 1.5/12,'часа.')
